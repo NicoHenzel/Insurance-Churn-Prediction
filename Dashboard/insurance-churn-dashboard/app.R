479be4d70e0551b49655de33ef89e2b4be62de77
NicoHenzel <nh103@hdm-stuttgart.de>
2022-05-08 22:26:43 +0200

    updated dashboard structure

--- Dashboard/insurance-churn-dashboard/app.R
+++ Dashboard/insurance-churn-dashboard/app.R
@@ -1,12 +1,10 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is a mockup for a dashboard for case handlers.
+# The goal is to give them an easy access to informations about insurance customer churn data.
+# In a real world scenario this dashboard would be integrated into existing applications of companies.
+# The data is taken from a classification model found here:
+# https://github.com/NicoHenzel/Insurance-Churn-Prediction
 
+# OFFEN
 # Tabelle mit Predictions übernehmen
 # Abbildungen: 
 # 1. Pie Chart Customer Churn distribution übernehmen auf neuem Datenset
@@ -16,35 +14,105 @@
 library(shiny) 
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  
+header <- dashboardHeader(
+    title = "Insurance churn",
+    dropdownMenu(
+      type = "notifications",
+      notificationItem(
+        text = "New contract data available",
+        icon("file")
+        )
+      ),
+    dropdownMenu(
+      type = "tasks",
+      badgeStatus = "success",
+      taskItem(
+        value = 50,
+        color = "blue",
+        "Contact clients"
+        )
+      )
     )
-  ),
-  dashboardBody(
+
+sidebar <-  dashboardSidebar(
+    sidebarMenu(
+      menuItem(
+        "Dashboard",
+        tabName = "dashboard",
+        icon = icon("dashboard")
+        ),
+      menuItem(
+        "Charts",
+        tabName = "charts",
+        icon = icon("chart-bar")
+        ),
+      menuItem(
+        "Source code", 
+        icon = icon("file-code-o"), 
+        href = "https://github.com/NicoHenzel/Insurance-Churn-Prediction/tree/main/Dashboard/insurance-churn-dashboard")
+    )
+  )
+
+body <- dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
+      tabItem(
+        tabName = "dashboard",
+        h2("Overview of contract churn data"),
+        fluidRow(
+        box(
+          title = tagList("Table of contract data", shiny::icon("file")),
+          color = "green",
+          plotOutput("plot1", height = 250)
+          ),
+        box(
+          title = "Controls",
+          sliderInput(
+            "slider",
+            "Number of observations:", 1, 100, 50)
+          )
+        ),
+        fluidRow(
+          # Dynamic valueBoxes
+          valueBoxOutput("clientBox")
+        ),
+        fluidRow(
+          # Clicking this will increment the progress amount
+          box(width = 4, actionButton("count", "Increment progress"))
+        )
+        
       ),
-
       # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+      tabItem(
+        tabName = "charts",
+        h2("Charts of contract data"),
+        fluidRow(
+          box(
+            title = tagList("Total churn distribution", shiny::icon("chart-pie")),
+            color = "blue",
+            plotOutput("plot2", height = 250)
+          ),
+          box(
+            title = tagList("Most important features", shiny::icon("exclamation")),
+            color = "blue",
+            plotOutput("plot3", height = 250)
+          ),
+          tabBox(
+            # Title can include an icon
+            title = tagList( "Bar charts", shiny::icon("chart-bar")),
+            tabPanel("Feature 1",
+                     "Tab content 1"
+            ),
+            tabPanel("Feature 2", "Tab content 2")
+          )
+        )
       )
     )
   )
-)
+
+
+ui <- dashboardPage(header, sidebar, body)
 
 server <- function(input, output) {
   set.seed(42)
@@ -54,49 +122,14 @@ server <- function(input, output) {
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
+  
+  output$clientBox <- renderValueBox({
+      valueBox(
+        paste0(42 + input$count), "Contaced clients", icon = icon("user"),
+        color = "purple"
+      )
+    })
 }
 
 shinyApp(ui, server)
 
-
-
-
-# 
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-#     })
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
